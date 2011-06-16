0cf8234cc2d37564c9ab94d5acea6800a5336b82
posativ <info@posativ.org>
2011-06-16 21:36:22 +0200

    implemented article overview

---
+++ ext/articles.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+import tools, os.path
+
+from copy import deepcopy
+from collections import defaultdict
+from jinja2 import Template
+from shpaml import convert_text
+
+def cb_end(request):
+    """Generates a overview of all articles."""
+    
+    request = deepcopy(request)
+    tools.run_callback('prepare', request) # needed for entry's url
+    
+    config = request._config
+    data = request._data
+    tt_articles = Template(convert_text(open('layouts/articles.html').read()))
+    
+    articles = defaultdict(list)
+    
+    for entry in sorted(data['entry_list'], key=lambda k: k._date, reverse=True):
+        url, title, year = entry['url'], entry['title'], entry._date.year
+        
+        articles[year].append((entry._date, url, title))
+        
+    articlesdict =config.copy()
+    articlesdict.update({'articles': articles,
+                 'num_entries': len(data['entry_list'])})
+                 
+    html = tt_articles.render(articlesdict)
+    path = os.path.join(config.get('output_dir', 'out'), 'articles', 'index.html')
+    
+    tools.mk_file(html, {'title': 'articles/index.html'}, path)
