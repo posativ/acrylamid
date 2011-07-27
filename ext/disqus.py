ac10861ccadf1e734ee826d74c581c408bd42e3d
posativ <info@posativ.org>
2011-07-27 10:08:06 +0200

    fixed var typo

--- ext/disqus.py
+++ ext/disqus.py
@@ -13,14 +13,14 @@ tt_item = """
     /* * * DON'T EDIT BELOW THIS LINE * * */
     (function() {
         var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-        dsq.src = '{{ protocoll }}://' + disqus_shortname + '.disqus.com/embed.js';
+        dsq.src = '{{ protocol }}://' + disqus_shortname + '.disqus.com/embed.js';
         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
     })();
 </script>
 <noscript>
     <p>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></p>
 </noscript>
-<a href="{{ protocoll }}://disqus.com" class="dsq-brlink">
+<a href="{{ protocol }}://disqus.com" class="dsq-brlink">
     blog comments powered by <span class="logo-disqus">Disqus</span>
 </a>""".strip()
 
@@ -34,7 +34,7 @@ tt_script = """
     (function () {
         var s = document.createElement('script'); s.async = true;
         s.type = 'text/javascript';
-        s.src = '{{ protocoll }}://' + disqus_shortname + '.disqus.com/count.js';
+        s.src = '{{ protocol }}://' + disqus_shortname + '.disqus.com/count.js';
         (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
     }());
 </script>""".strip()
