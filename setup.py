baf15f8700c3315a6edd08aba037eba3c4e21204
posativ <info@posativ.org>
2011-11-16 20:31:00 +0100

    distribute

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from distutils.core import setup
 
 setup(
-    name='lilith',
-    version='0.1.7',
+    name='acrylamid',
+    version='0.1.10',
     author='posativ',
     author_email='info@posativ.org',
-    packages=['lilith'],
-    scripts=['bin/lilith'],
-    #scripts=['bin/stowe-towels.py','bin/wash-towels.py'],
-    url='http://pypi.python.org/pypi/lilith/',
+    packages=['acrylamid'],
+    scripts=['bin/acrylamid'],
+    url='http://pypi.python.org/pypi/acrylamid/',
     license='LICENSE.txt',
     description='yet another static blog generator.',
     long_description=open('README.txt').read(),
+    install_requires=[
+        "jinja2 == 2.4.1",
+        "Markdown >= 2.0.3",
+    ],
 )
