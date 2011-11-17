0f6402abe6fc9deaf1d4af293869457f91fcac8e
posativ <info@posativ.org>
2011-11-17 11:18:00 +0100

    next try

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+import sys; reload(sys)
+sys.setdefaultencoding('utf-8')
+import codecs
 from distutils.core import setup
 
 setup(
@@ -5,14 +8,20 @@ setup(
     version='0.1.10',
     author='posativ',
     author_email='info@posativ.org',
-    packages=['acrylamid'],
+    packages=['acrylamid', 'acrylamid.filters', 'acrylamid.views'],
     scripts=['bin/acrylamid'],
     url='http://pypi.python.org/pypi/acrylamid/',
     license='LICENSE.txt',
     description='yet another static blog generator.',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "jinja2 == 2.4.1",
-        "Markdown >= 2.0.3",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Internet",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+    ],
+   install_requires=[
+        'Jinja2>=2.4',
+        'Markdown>=2.0.1'
     ],
 )
