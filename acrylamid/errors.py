ddb6d7b5b335098f5a811cc08d9c8104bb62aa3e
posativ <info@posativ.org>
2012-03-03 18:05:37 +0400

    RSS 2.0 import

--- acrylamid/errors.py
+++ acrylamid/errors.py
@@ -3,8 +3,6 @@
 # Copyright 2011 posativ <info@posativ.org>. All rights reserved.
 # License: BSD Style, 2 clauses. see acrylamid.py
 
-class AcrylamidException(Exception):
-	pass
 
-class AcrylamidWarning(Warning):
+class AcrylamidException(Exception):
 	pass
