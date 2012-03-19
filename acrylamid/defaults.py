3372bbbb339fc51fd0d78045203425c2c7386424
posativ <info@posativ.org>
2012-03-20 00:54:40 +0600

    env. in front of protocol to make Disqus work again

--- acrylamid/defaults.py
+++ acrylamid/defaults.py
@@ -503,7 +503,7 @@ entry = r'''
             /* * * DON'T EDIT BELOW THIS LINE * * */
             (function() {
                 var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-                dsq.src = '{{ protocol }}://' + disqus_shortname + '.disqus.com/embed.js';
+                dsq.src = '{{ env.protocol }}://' + disqus_shortname + '.disqus.com/embed.js';
                 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
             })();
         </script>
