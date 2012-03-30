8d3e33878dd2c81ff4a6af6e6625fc20aad759a4
posativ <info@posativ.org>
2012-03-31 00:40:57 +0200

    update Development Status -> Stable :-)

--- setup.py
+++ setup.py
@@ -1,88 +1,120 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-"""acrylamid documentation
-=======================
+"""Welcome to Acrylamid's Documentation!
+=====================================
 
-acrylamid is yet another lightweight static blogging software written in python
-and designed to get a high quality output. Its licensed under BSD Style, 2 clauses.
+Acrylamid is yet another lightweight static blogging software written in
+python and designed to get a high quality output. Its licensed under BSD
+Style, 2 clauses.
 
 Features
-********
+--------
 
-- blog articles, pages and rss/atom feeds
-- theming support (using jinja2_)
-- Markdown_ and reStructuredText_ as markup languages
-- MathML generation using AsciiMathML_
-- hyphenation using `&shy;`
-- modern web-typography
+Acrylamid is a mixture of `nanoc <http://nanoc.stoneship.org/>`_, `Pyblosxom
+<http://pyblosxom.bluesock.org/>`_ and `Pelican <http://pelican.notmyidea.org/>`_. It
+features mainly:
+
+- blog articles, simple pages, tags RSS/Atom feeds and an article overview
+- theming support (using jinja2_) and support for jinja2 directly in postings
+- Markdown_, reStructuredText_ and pandoc_
+- MathML, modern web-typography and hyphenation using `&shy;`
+- RSS/Atom import, deployment and a very handy CLI
+- it's very flexible and fast
 
 .. _jinja2: http://jinja.pocoo.org/
 .. _reStructuredText: http://docutils.sourceforge.net/rst.html
 .. _Markdown: http://daringfireball.net/projects/markdown/
+.. _pandoc: http://johnmacfarlane.net/pandoc/
 .. _AsciiMathML: http://www1.chapman.edu/~jipsen/mathml/asciimath.html
 
 Quickstart
-**********
+----------
 
 ::
 
-    pip install acrylamid
+    easy_install -U acrylamid
 
 You'll need ``python``, ``jinja2`` and either ``markdown`` (default) or
 ``docutils``. ``pygments`` and ``asciimathml`` for colored code listings
-respectively MathML. Typography needs ``smartypants``. To get a full-featured
-installation do:
+respectively MathML. Typography requires ``smartypants``. To get a
+full-featured installation do:
 
 ::
 
     pip install docutils pygments asciimathml smartypants
 
-Get acrylamid and edit *conf.yaml* and *layouts/*. Run acrylamid with:
+Get acrylamid, edit *conf.py* and *layouts/* and run acrylamid with:
 
 ::
 
     $> acrylamid init myblog
-        create  myblog/content/
+        create  myblog/conf.py
         ...
     $> cd myblog/
-    $> acrylamid gen
+    $> acrylamid compile && acrylamid view
           warn  using mtime from <fileentry f'content/sample entry.txt'>
         create  '/articles/index.html', written to output/articles/index.html
         create  'Die Verwandlung', written to output/2011/die-verwandlung/index.html
         create  '/atom/index.html', written to output/atom/index.html
         create  '/rss/index.html', written to output/rss/index.html
         create  '/', written to output/index.html
+       * Running on http://127.0.0.1:8000/
 
-Using acrylamid
-***************
+Real World Example?
+*******************
 
-- `conf.yaml <https://github.com/posativ/acrylamid/blob/master/docs/conf.yaml.rst>`_
-- `filters <https://github.com/posativ/acrylamid/blob/master/docs/filters.rst>`_
+I have released all sources from my personal blog:
+`posativ/blog.posativ.org <https://github.com/posativ/blog.posativ.org>`_. Take a
+look at my *conf.py* for some inspiration.
+
 
 Filters
-**********
+-------
 
-See `docs/filters.rst
-<https://github.com/posativ/acrylamid/blob/master/docs/filters.rst>`_ for
+See `docs/filters.rst <http://acrylamid.readthedocs.org/en/latest/filters.html>`_ for
 detailed information. Currently supported by acrylamid:
 
-- **markdown**: rendering Markdown (+asciimathml,pygments)
-- **rest**: reStructuredText (+pygments)
+- **Markdown**: rendering Markdown (+asciimathml,pygments,built-in extensions)
+- **reST**: reStructuredText (+pygments)
+- **pandoc**: Pandoc (+Markdown,textitle,rst,...)
+- **HTML**: don't render with Markdown, reStructuredText or Pandoc (it's a conflicting filter)
+
 - **typography**: https://code.google.com/p/typogrify/ (and custom modifications)
-- **summarize**: summarizes posts to 200 words
 - **hyphenation**: hyphenate words (len > 10) based on language
+- **summarize**: summarizes posts to 200 words
+
 - **head_offset**: decrease headings by offset
-- **pass-through**: don't render with Markdown or reStructuredText
+- **jinja2**: write jinja2 in your entries (you can also execute system calls therewith)
+- **acronyms**: automatically replace acronyms and abbreviations to help unexperienced users
 
-Views
-*****
+Commands
+--------
 
-- **articles**: articles overview
-- **entry**: renders single entry to given slug
-- **index**: creates pagination / and /page/:num
-- **feeds**: valid atom/rss feed
-- **tags**: sort by tag with pagination (/:tag and /:tag/:num)
+See :doc:`commands` for a detailed overview.
+
+::
+
+    %> acrylamid --help
+    Usage: acrylamid <subcommand> [options] [args]
+
+    Options:
+      -q, --quiet    less verbose
+      -v, --verbose  more verbose
+      -h, --help     show this help message and exit
+      --version      print version details
+
+    Commands:
+      init           initializes base structure in DIR
+      create  (new)  creates a new entry
+      compile (co)   compile blog
+      view           fire up built-in webserver
+      autocompile    automatic compilation and serving (short aco)
+      clean   (rm)   remove abandoned files
+      import         import content from URL
+      deploy         run a given TASK
+
+    All subcommands except `init` require a conf.py file.
 """
 
 setup(
@@ -90,7 +122,7 @@ setup(
     version='0.3.0',
     author='posativ',
     author_email='info@posativ.org',
-    packages=['acrylamid', 'acrylamid.filters', 'acrylamid.views'],
+    packages=['acrylamid', 'acrylamid.filters', 'acrylamid.views', 'acrylamid.lib'],
     scripts=['bin/acrylamid'],
     package_data={'acrylamid.filters': ['hyph/*.txt']},
     url='http://pypi.python.org/pypi/acrylamid/',
@@ -98,7 +130,7 @@ setup(
     description='yet another static blog generator',
     long_description=__doc__,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Topic :: Internet",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary",
         "Environment :: Console",
