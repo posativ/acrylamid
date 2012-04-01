posativ/acrylamid

commit 07e17567e8d29052f3f83fedf5b25ebffb913b77

rework view API
This required a lot of changes including change to the core compiler
routine and many little bugfixes:

- using utils.paginate and has_changed property which fixes some issues when
  a view should re-render pages

- should improve performance since it must no longer evaluate the whole page
  but can just pass this page if neither the template, nor the file entry or
  entry list has changed.

- include Tagcloud into global environment object and update default settings.

New API is defined as followed:

- `def init(self, foo=bar): pass` is the same as pelican's context and can be
  used to initialize some config variables and so on. This is evaluated even
  if no entry gets actually rendered.

- `def context(self, env, request): return env` is a new callback that lets you
  modify the context before any view gets rendered. This should be used to include
  helper objects usable in every view.

- `def generate(self, request): yield html, path (, optional message)` is completely
  reworked so you don't repeat yourself every time. Just return the HTML and a path
  and acrylamid will create a file if necessary and also does time measurements.

acrylamid/commands.py
@@ -17,7 +17,7 @@ from urlparse import urlsplit
 from datetime import datetime
 from jinja2 import Environment, FileSystemBytecodeCache
 
-from acrylamid import filters, views, log
+from acrylamid import filters, views, log, utils
 from acrylamid.lib.importer import fetch, parse, build
 from acrylamid.utils import cache, ExtendedFileSystemLoader, FileEntry, event, escapes, system
 from acrylamid.errors import AcrylamidException
@@ -87,9 +87,13 @@ def compile(conf, env, force=False, **options):
         cache.clear()
 
     entrylist = request.pop('entrylist')
-    filtersdict = get_filters()
+    filtersdict = get_filters()  # dict = {'fname' : function}
     _views = get_views()
 
+    for v in _views:
+        env = v.context(env, {'entrylist': entrylist})
+
+    # don't touch this! It works, but it's a pain without explicit references.
     for v in _views:
         log.debug(v)
         request['entrylist'] = []
@@ -106,15 +110,25 @@ def compile(conf, env, force=False, **options):
 
             _filters = FilterList()
             for f in entryfilters + viewsfilters:
-                x, y = f.split('+')[:1][0], f.split('+')[1:]
-                if filtersdict[x] not in _filters:
-                    _filters.append((x, filtersdict[x], y))
+                fname, fargs = f.split('+')[:1][0], f.split('+')[1:]
+                if filtersdict[fname] not in _filters:
+                    _filters.append((fname, filtersdict[fname], fargs))
 
             entry.lazy_eval = _filters
             request['entrylist'].append(entry)
 
         request['entrylist'] = filter(v.condition, entrylist)
-        v(request, **options)
+
+        tt = time.time()
+        for res in v.generate(request):
+            try:
+                html, path, message = res
+            except ValueError:
+                # allow two items yielding for simplicity
+                html, path = res
+                message = path.replace(conf['output_dir'], '')
+            utils.mkfile(html, path, message, time.time()-tt, **options)
+
     log.info('Blog compiled in %.2fs' % (time.time() - ctime))
 
 

acrylamid/views/articles.py
@@ -2,9 +2,9 @@
 # License: BSD Style, 2 clauses. see acrylamid.py
 
 from acrylamid.views import View
-from acrylamid.utils import union, mkfile, joinurl, event
+from acrylamid.utils import union, joinurl, event, cache, md5
 
-from os.path import getmtime, exists
+from os.path import exists
 from collections import defaultdict
 
 
@@ -13,35 +13,41 @@ class Articles(View):
 
     path = '/articles/'
 
-    def __init__(self, conf, env, *args, **kwargs):
-        View.__init__(self, *args, **kwargs)
+    def generate(self, request):
 
-    def __call__(self, request, *args, **kwargs):
-
-        conf = request['conf']
-        env = request['env']
-        entrylist = request['entrylist']
+        entrylist = sorted((e for e in request['entrylist'] if not e.draft),
+                        key=lambda k: k.date, reverse=True)
 
         articles = defaultdict(list)
-        tt_articles = env['tt_env'].get_template('articles.html')
+        tt_articles = self.env['tt_env'].get_template('articles.html')
 
-        p = joinurl(conf['output_dir'], self.path)
+        p = joinurl(self.conf['output_dir'], self.path)
         if not filter(lambda e: p.endswith(e), ['.xml', '.html']):
             p = joinurl(p, 'index.html')
 
-        last_modified = max([getmtime(e.filename) for e in entrylist]) if entrylist else 2**32
-        if exists(p) and last_modified < getmtime(p):
-            if not tt_articles.has_changed:
-                event.skip(p.replace(conf['output_dir'], ''), path=p)
-                return
+        hv = md5(*entrylist, attr=lambda o: o.md5)
+        rv = cache.memoize('articles-hash')
 
-        for entry in sorted(entrylist, key=lambda k: k.date, reverse=True):
-            if entry.draft:
-                continue
+        if rv == hv:
+            # check if a FileEntry-instance has changed
+            if bool(filter(lambda e: e.has_changed, entrylist)):
+                has_changed = True
+            else:
+                has_changed = False
+        else:
+            # save new value for next run
+            cache.memoize('articles-hash', hv)
+            has_changed = True
 
+        if exists(p) and not has_changed and not tt_articles.has_changed:
+                event.skip(p.replace(self.conf['output_dir'], ''), path=p)
+                raise StopIteration
+
+        for entry in entrylist:
             url, title, year = entry.permalink, entry.title, entry.date.year
             articles[year].append((entry.date, url, title))
 
-        html = tt_articles.render(conf=conf, articles=articles,
-                                  env=union(env, num_entries=len(entrylist)))
-        mkfile(html, p, p.replace(conf['output_dir'], ''), **kwargs)
+        html = tt_articles.render(conf=self.conf, articles=articles,
+                                  env=union(self.env, num_entries=len(entrylist)))
+
+        yield html, p

acrylamid/views/entry.py
@@ -4,7 +4,7 @@
 from os.path import exists
 
 from acrylamid.views import View
-from acrylamid.utils import expand, union, mkfile, joinurl, event
+from acrylamid.utils import expand, union, joinurl, event
 from acrylamid.errors import AcrylamidException
 
 class Entry(View):
@@ -13,24 +13,19 @@ class Entry(View):
     path = '/:year/:slug/'
     filters = []
 
-    def __init__(self, conf, env, *args, **kwargs):
-        View.__init__(self, *args, **kwargs)
+    def generate(self, request):
 
-    def __call__(self, request, *args, **kwargs):
-
-        conf = request['conf']
-        env = request['env']
         entrylist = request['entrylist']
 
-        tt_entry = env['tt_env'].get_template('entry.html')
-        tt_main = env['tt_env'].get_template('main.html')
+        tt_entry = self.env['tt_env'].get_template('entry.html')
+        tt_main = self.env['tt_env'].get_template('main.html')
         pathes = dict()
 
         for entry in entrylist:
             if entry.permalink != expand(self.path, entry):
-                p = joinurl(conf['output_dir'], entry.permalink)
+                p = joinurl(self.conf['output_dir'], entry.permalink)
             else:
-                p = joinurl(conf['output_dir'], expand(self.path, entry))
+                p = joinurl(self.conf['output_dir'], expand(self.path, entry))
 
             if not filter(lambda e: p.endswith(e), ['.xml', '.html']):
                 p = joinurl(p, 'index.html')
@@ -46,7 +41,7 @@ class Entry(View):
                     event.skip(entry.title, path=p)
                     continue
 
-            html = tt_main.render(env=union(env, entrylist=[entry], type='entry'),
-                                  conf=conf, entry=entry)
+            html = tt_main.render(env=union(self.env, entrylist=[entry], type='entry'),
+                                  conf=self.conf, entry=entry)
 
-            mkfile(html, p, entry.title, ctime=entry.ctime, **kwargs)
+            yield html, p, entry.title

acrylamid/views/index.py
@@ -2,48 +2,31 @@
 # License: BSD Style, 2 clauses. see acrylamid.py
 # -*- encoding: utf-8 -*-
 
-from time import time
 from os.path import exists
 
-from acrylamid import log
 from acrylamid.views import View
-from acrylamid.utils import union, mkfile, joinurl, event, paginate, expand, cache
+from acrylamid.utils import union, joinurl, event, paginate, expand
 
 
 class Index(View):
 
-    def __init__(self, conf, env, items_per_page=10, pagination='/page/:num/', *args, **kwargs):
-        View.__init__(self, *args, **kwargs)
+    def init(self, items_per_page=10, pagination='/page/:num/'):
         self.items_per_page = items_per_page
         self.pagination = pagination
 
-    def __call__(self, request, *args, **kwargs):
+    def generate(self, request):
         """Creates nicely paged listing of your posts.  First page is the
         index.hml used to have this nice url: http://yourblog.com/ with a recent
         list of your (e.g. summarized) Posts. Other pages are enumerated to /page/n+1
         """
 
-        conf = request['conf']
-        env = request['env']
         entrylist = request['entrylist']
         ipp = self.items_per_page
 
-        tt_entry = env['tt_env'].get_template('entry.html')
-        tt_main = env['tt_env'].get_template('main.html')
+        tt_entry = self.env['tt_env'].get_template('entry.html')
+        tt_main = self.env['tt_env'].get_template('main.html')
 
-        pages, has_changed = paginate(entrylist, ipp, lambda e: not e.draft)
-
-        try:
-            rv = cache.memoize('index-hash')
-            has_changed = False
-            if rv != [repr(e) for e in entrylist if not e.draft]:
-                raise KeyError
-        except KeyError:
-            cache.memoize('index-hash', [repr(e) for e in entrylist if not e.draft])
-            has_changed = True
-
-        for i, entries in enumerate(pages):
-            ctime = time()
+        for i, entries, has_changed in paginate(entrylist, ipp, lambda e: not e.draft):
 
             # curr = current page, next = newer pages, prev = older pages
             if i == 0:
@@ -52,26 +35,26 @@ class Index(View):
             else:
                 curr = expand(self.pagination, {'num': str(i+1)})
                 next = self.path if i == 1 else expand(self.pagination, {'num': str(i)})
-            prev = None if i >= len(list(pages))-1 \
+            prev = None if i >= len(list(entries))-1 \
                         else expand(self.pagination, {'num': str(i+2)})
 
-            directory = conf['output_dir']
+            directory = self.conf['output_dir']
             if i == 0:
-                directory = joinurl(conf['output_dir'], self.path)
+                directory = joinurl(self.conf['output_dir'], self.path)
             else:
-                directory = joinurl(conf['output_dir'],
+                directory = joinurl(self.conf['output_dir'],
                                 expand(self.pagination, {'num': str(i+1)}))
 
             p = joinurl(directory, 'index.html')
             message = self.path if i==0 else expand(self.pagination, {'num': str(i+1)})
 
-            if exists(p) and not has_changed and not entrylist.has_changed:
+            if exists(p) and not has_changed:
                 if not (tt_entry.has_changed or tt_main.has_changed):
                     event.skip(message, path=p)
                     continue
 
-            html = tt_main.render(conf=conf, env=union(env, entrylist=entries, type='index',
-                                    prev=prev, curr=curr, next=next,  items_per_page=ipp,
-                                    num_entries=len(entrylist)))
+            html = tt_main.render(conf=self.conf, env=union(self.env, entrylist=entries,
+                                  type='index', prev=prev, curr=curr, next=next,
+                                  items_per_page=ipp, num_entries=len(entrylist)))
 
-            mkfile(html, p, message, ctime=time()-ctime, **kwargs)
+            yield html, p, message

commit 794ece487d244b8fd8de297c5b6a56a09d0876c1

fix pagination regression from 07e17567
- utils.paginate emits a tuple of (next, current, previous) instead
  of a single index since during the yield statement a view can not
  estimate how many pages will be emitted.

- I also removed the `what` argument in utils.event, since it is not
  used in output therefore senseless.

- tag-view now has 10 items per page as default

These changes include little changes to main.html.

acrylamid/commands.py
@@ -46,7 +46,7 @@ def initialize(conf, env):
         else:
             locale.setlocale(locale.LC_ALL, '')
             log.debug("locale set to '%s'", locale.getlocale()[0])
-    conf['lang'] = locale.getlocale()[0]
+    conf['lang'] = locale.getlocale()[0][:2]
 
     if 'www_root' not in conf:
         log.warn('no `www_root` specified, using localhost:8000')
@@ -164,15 +164,8 @@ def compile(conf, env, force=False, **options):
         request['entrylist'] = filter(v.condition, entrylist)
         tt = time.time()
 
-        for res in v.generate(request):
-            try:
-                html, path, message = res
-            except ValueError:
-                # also allow two items yielding for simplicity
-                html, path = res
-                message = path.replace(conf['output_dir'], '')
-
-            utils.mkfile(html, path, message, time.time()-tt, **options)
+        for html, path in v.generate(request):
+            utils.mkfile(html, path, time.time()-tt, **options)
             tt = time.time()
 
     log.info('Blog compiled in %.2fs' % (time.time() - ctime))

acrylamid/defaults/html5/main.html
@@ -22,12 +22,12 @@
     {% endfor %}
     {% if env.type in ['tag', 'index'] %}
         {% if env.prev %}
-            <a href="{{ env.path + env.prev + '/' }}" class="page floatright">
+            <a href="{{ env.path ~ env.prev ~ '/' }}" class="page floatright">
             ältere Beiträge →
             </a>
         {% endif %}
-        {% if env.next %}
-            <a href="{{ env.path + env.next + '/' }}" class="page floatleft">
+        {% if env.next is not none %}
+            <a href="{{ env.path ~ env.next ~ '/' }}" class="page floatleft">
             ← neuere Beiträge
             </a>
         {% endif %}

acrylamid/defaults/xhtml/main.html
@@ -22,12 +22,12 @@
     {% endfor %}
     {% if env.type in ['tag', 'index'] %}
         {% if env.prev %}
-            <a href="{{ env.path + env.prev + '/' }}" class="page floatright">
+            <a href="{{ env.path ~ env.prev ~ '/' }}" class="page floatright">
             ältere Beiträge →
             </a>
         {% endif %}
-        {% if env.next %}
-            <a href="{{ env.path + env.next + '/' }}" class="page floatleft">
+        {% if env.next is not none %}
+            <a href="{{ env.path ~ env.next ~ '/' }}" class="page floatleft">
             ← neuere Beiträge
             </a>
         {% endif %}

acrylamid/utils.py
@@ -504,12 +504,11 @@ def union(*args, **kwargs):
     return d
 
 
-def mkfile(content, path, message, ctime=0.0, force=False, dryrun=False, **kwargs):
+def mkfile(content, path, ctime=0.0, force=False, dryrun=False, **kwargs):
     """Creates entry in filesystem. Overwrite only if content differs.
 
     :param content: rendered html/xml to write
     :param path: path to write to
-    :param message: message to display
     :param ctime: time needed to compile
     :param force: force overwrite, even nothing has changed (defaults to `False`)
     :param dryrun: don't write anything."""
@@ -519,12 +518,12 @@ def mkfile(content, path, message, ctime=0.0, force=False, dryrun=False, **kwarg
         with file(path) as f:
             old = f.read()
         if content == old and not force:
-            event.identical(message, path=path)
+            event.identical(path=path)
         else:
             if not dryrun:
                 with open(path, 'w') as f:
                     f.write(content)
-            event.changed(message, path=path, ctime=ctime)
+            event.changed(path=path, ctime=ctime)
     else:
         try:
             if not dryrun:
@@ -535,7 +534,7 @@ def mkfile(content, path, message, ctime=0.0, force=False, dryrun=False, **kwarg
         if not dryrun:
             with open(path, 'w') as f:
                 f.write(content)
-        event.create(message, path=path, ctime=ctime)
+        event.create(path=path, ctime=ctime)
 
 
 def md5(*objs,  **kw):
@@ -607,7 +606,13 @@ def paginate(list, ipp, func=lambda x: x, salt=None, orphans=0):
         res[-2].extend(res[-1])
         res.pop(-1)
 
+    j = len(res)
     for i, entries in enumerate(res):
+
+        i += 1
+        next = None if i == 1 else i-1
+        curr = i
+        prev = None if i >= j else i+1
 
         # get caller, so we can set a unique and meaningful hash-key
         frame = log.findCaller()
@@ -631,7 +636,7 @@ def paginate(list, ipp, func=lambda x: x, salt=None, orphans=0):
             cache.memoize(hkey, hv)
             has_changed = True
 
-        yield i, entries, has_changed
+        yield (next, curr, prev), entries, has_changed
 
 
 def escape(string):
@@ -797,10 +802,10 @@ class cache(object):
 
 def track(f):
     """decorator to track files when event.create|change|skip is called."""
-    def dec(cls, what, path, *args, **kwargs):
+    def dec(cls, path, *args, **kwargs):
         global _tracked_files
         _tracked_files.add(path)
-        return f(cls, what, path, *args, **kwargs)
+        return f(cls, path, *args, **kwargs)
     return dec
 
 
@@ -824,7 +829,7 @@ class event:
 
     @classmethod
     @track
-    def create(self, what, path, ctime=None):
+    def create(self, path, ctime=None):
         if ctime:
             log.info("create  [%.2fs] %s", ctime, path)
         else:
@@ -832,7 +837,7 @@ class event:
 
     @classmethod
     @track
-    def changed(self, what, path, ctime=None):
+    def changed(self, path, ctime=None):
         if ctime:
             log.info("update  [%.2fs] %s", ctime, path)
         else:
@@ -840,12 +845,12 @@ class event:
 
     @classmethod
     @track
-    def skip(self, what, path):
+    def skip(self, path):
         log.skip("skip  %s", path)
 
     @classmethod
     @track
-    def identical(self, what, path):
+    def identical(self, path):
         log.skip("identical  %s", path)
 
     @classmethod

acrylamid/views/articles.py
@@ -17,10 +17,7 @@ class Articles(View):
                         key=lambda k: k.date, reverse=True)
 
         tt = self.env.jinja2.get_template('articles.html')
-
-        p = joinurl(self.conf['output_dir'], self.path)
-        if not filter(lambda e: p.endswith(e), ['.xml', '.html']):
-            p = joinurl(p, 'index.html')
+        path = joinurl(self.conf['output_dir'], self.path, 'index.html')
 
         hv = md5(*entrylist, attr=lambda o: o.md5)
         rv = cache.memoize('articles-hash')
@@ -32,8 +29,8 @@ class Articles(View):
             cache.memoize('articles-hash', hv)
             has_changed = True
 
-        if exists(p) and not has_changed and not tt.has_changed:
-                event.skip(p.replace(self.conf['output_dir'], ''), path=p)
+        if exists(path) and not has_changed and not tt.has_changed:
+                event.skip(path)
                 raise StopIteration
 
         articles = {}
@@ -43,4 +40,4 @@ class Articles(View):
         html = tt.render(conf=self.conf, articles=articles,
                          env=union(self.env, num_entries=len(entrylist)))
 
-        yield html, p
+        yield html, path

acrylamid/views/entry.py
@@ -31,12 +31,12 @@ class Entry(View):
                                                                        entry.filename))
             pathes[p] = entry
 
-        for p, entry in pathes.iteritems():
-            if exists(p) and not entry.has_changed and not tt.has_changed:
-                event.skip(entry.title, path=p)
+        for path, entry in pathes.iteritems():
+            if exists(path) and not entry.has_changed and not tt.has_changed:
+                event.skip(path)
                 continue
 
             html = tt.render(env=union(self.env, entrylist=[entry], type='entry'),
                              conf=self.conf, entry=entry)
 
-            yield html, p, entry.title
+            yield html, path

acrylamid/views/feeds.py
@@ -16,21 +16,21 @@ class Feed(View):
         entrylist = filter(lambda e: not e.draft, request['entrylist'])[:self.num_entries]
         tt = self.env.jinja2.get_template('%s.xml' % self.__class__.__name__)
 
-        p = joinurl(self.conf['output_dir'], self.path)
-        if not filter(lambda e: p.endswith(e), ['.xml', '.html']):
-            p = joinurl(p, 'index.html')
+        path = joinurl(self.conf['output_dir'], self.path)
+        if not filter(lambda e: path.endswith(e), ['.xml', '.html']):
+            path = joinurl(path, 'index.html')
 
 
-        if exists(p) and not filter(lambda e: e.has_changed, entrylist):
+        if exists(path) and not filter(lambda e: e.has_changed, entrylist):
             if not tt.has_changed:
-                event.skip(p.replace(self.conf['output_dir'], ''), path=p)
+                event.skip(path)
                 raise StopIteration
 
         updated=entrylist[0].date if entrylist else datetime.now()
         html = tt.render(conf=self.conf, env=union(self.env,
                          updated=updated, entrylist=entrylist))
 
-        yield html, p
+        yield html, path
 
 
 class Atom(Feed):

acrylamid/views/index.py
@@ -23,36 +23,26 @@ class Index(View):
         ipp = self.items_per_page
         tt = self.env.jinja2.get_template('main.html')
 
-        entrylist = request['entrylist']
-        for i, entries, has_changed in paginate(entrylist, ipp, lambda e: not e.draft,
-                                                orphans=self.conf['default_orphans']):
+        entrylist = [entry for entry in request['entrylist'] if not entry.draft]
+        paginator = paginate(entrylist, ipp, orphans=self.conf['default_orphans'])
 
+        for (next, curr, prev), entries, has_changed in paginator:
             # curr = current page, next = newer pages, prev = older pages
-            if i == 0:
-                next = None
-                curr = self.path
-            else:
-                curr = expand(self.pagination, {'num': str(i+1)})
-                next = self.path if i == 1 else expand(self.pagination, {'num': str(i)})
-            prev = None if i >= len(list(entries))-1 \
-                        else expand(self.pagination, {'num': str(i+2)})
 
-            directory = self.conf['output_dir']
-            if i == 0:
-                directory = joinurl(self.conf['output_dir'], self.path)
-            else:
-                directory = joinurl(self.conf['output_dir'],
-                                expand(self.pagination, {'num': str(i+1)}))
+            if next is not None:
+                next = self.path.rstrip('/') if next == 1 \
+                           else expand(self.pagination, {'num': next})
 
-            p = joinurl(directory, 'index.html')
-            message = self.path if i==0 else expand(self.pagination, {'num': str(i+1)})
+            curr = self.path if curr == 1 else expand(self.pagination, {'num': curr})
+            prev = None if prev is None else expand(self.pagination, {'num': prev})
+            path = joinurl(self.conf['output_dir'], curr, 'index.html')
 
-            if exists(p) and not has_changed and not tt.has_changed:
-                event.skip(message, path=p)
+            if exists(path) and not has_changed and not tt.has_changed:
+                event.skip(path)
                 continue
 
             html = tt.render(conf=self.conf, env=union(self.env, entrylist=entries,
                                   type='index', prev=prev, curr=curr, next=next,
                                   items_per_page=ipp, num_entries=len(entrylist)))
 
-            yield html, p, message
+            yield html, path

acrylamid/views/tag.py
@@ -46,7 +46,7 @@ class Tagcloud:
 
 class Tag(View):
 
-    def init(self, items_per_page=25, pagination='/tag/:name/:num/'):
+    def init(self, items_per_page=10, pagination='/tag/:name/:num/'):
         self.items_per_page = items_per_page
         self.pagination = pagination
 
@@ -85,32 +85,38 @@ class Tag(View):
         ipp = self.items_per_page
         tt = self.env.jinja2.get_template('main.html')
 
-        entrylist = request['entrylist']
+        entrylist = [entry for entry in request['entrylist'] if not entry.draft]
 
         for tag in self.tags:
+
             entrylist = [entry for entry in self.tags[tag]]
-            for i, entries, has_changed in paginate(entrylist, ipp, lambda e: not e.draft, salt=tag,
-                                                    orphans=self.conf['default_orphans']):
+            paginator = paginate(entrylist, ipp, salt=tag, orphans=self.conf['default_orphans'])
+
+            for (next, curr, prev), entries, has_changed in paginator:
 
                 # e.g.: curr = /page/3, next = /page/2, prev = /page/4
-                if i == 0:
-                    next = None
+
+                if next == 1:
+                    next = expand(self.path, {'name': tag}).rstrip('/')
+                elif next > 1:
+                    next = expand(self.pagination, {'name': tag, 'num': next})
+
+                if curr == 1:
                     curr = expand(self.path, {'name': tag})
-                else:
-                    curr = expand(self.pagination, {'num': str(i+1), 'name': tag})
-                    next = expand(self.path, {'name': tag}) if i==1 \
-                             else expand(self.pagination, {'name': tag, 'num': str(i)})
+                elif curr > 1:
+                    curr = expand(self.pagination, {'num': curr, 'name': tag})
 
-                prev = None if i >= len(list(entries))-1 \
-                            else expand(self.pagination, {'name': tag, 'num': str(i+2)})
-                p = joinurl(self.conf['output_dir'], curr, 'index.html')
+                if prev is not None:
+                    prev = expand(self.pagination, {'name': tag, 'num': prev})
 
-                if exists(p) and not has_changed and not tt.has_changed:
-                    event.skip(curr, path=p)
+                path = joinurl(self.conf['output_dir'], curr, 'index.html')
+
+                if exists(path) and not has_changed and not tt.has_changed:
+                    event.skip(path)
                     continue
 
                 html = tt.render(conf=self.conf, env=union(self.env, entrylist=entries,
                                 type='tag', prev=prev, curr=curr, next=next,
                                 items_per_page=ipp, num_entries=len(entrylist)))
 
-                yield html, p, curr
+                yield html, path

tests/test_utils.py
@@ -76,28 +76,28 @@ class TestUtils(unittest.TestCase):
 
         # default stuff
         self.assertEqual(list(utils.paginate(res, 4)),
-            [(0, res[:4], True), (1, res[4:], True)])
+            [((None, 1, 2), res[:4], True), ((1, 2, None), res[4:], True)])
         self.assertEqual(list(utils.paginate(res, 4, lambda x: x.isdigit())),
-            [(0, [X('1'), X('42')], True), ])
+            [((None, 1, None), [X('1'), X('42')], True), ])
         self.assertEqual(list(utils.paginate(res, 7)),
-            [(0, res[:7], True), (1, res[7:], True)])
+            [((None, 1, 2), res[:7], True), ((1, 2, None), res[7:], True)])
 
         # with orphans
         self.assertEqual(list(utils.paginate(res, 7, orphans=1)),
-            [(0, res, True)])
+            [((None, 1, None), res, True)])
         self.assertEqual(list(utils.paginate(res, 6, orphans=1)),
-            [(0, res[:6], True), (1, res[6:], True)])
+            [((None, 1, 2), res[:6], True), ((1, 2, None), res[6:], True)])
 
         # a real world example which has previously failed
         res = [X(_) for _ in range(20)]
         self.assertEqual(list(utils.paginate(res, 10)),
-            [(0, res[:10], True), (1, res[10:], True)])
+            [((None, 1, 2), res[:10], True), ((1, 2, None), res[10:], True)])
 
         # edge cases
         self.assertEqual(list(utils.paginate([], 2)), [])
         self.assertEqual(list(utils.paginate([], 2, orphans=7)), [])
         self.assertEqual(list(utils.paginate([X('1'), X('2'), X('3')], 3, orphans=1)),
-            [(0, [X('1'), X('2'), X('3')], True)])
+            [((None, 1, None), [X('1'), X('2'), X('3')], True)])
 
     def test_escape(self):
 