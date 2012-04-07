b32a2b1c5b19c9e4da4303f9bc94892aadb8030f
posativ <info@posativ.org>
2012-04-07 20:05:00 +0200

    add YouTube, Code (JavaScript), Sourcecode (Pygments) directives

---
+++ acrylamid/filters/rstx_code.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright: (c) 2010-2012 by Rafael Goncalves Martins
+# License: GPL-2, see https://hg.rafaelmartins.eng.br/blohg/file/tip/LICENSE for more details.
+
+from docutils import nodes
+from docutils.parsers.rst import Directive
+
+match = ['code']
+
+
+class Code(Directive):
+    """reStructuredText directive that creates a pre tag suitable for
+    decoration with http://alexgorbatchev.com/SyntaxHighlighter/
+
+    Usage example::
+
+        .. source:: python
+
+           print "Hello, World!"
+
+        .. raw:: html
+
+            <script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/current/scripts/shCore.js"></script>
+            <script type="text/javascript" src="http://alexgorbatchev.com/pub/sh/current/scripts/shBrushPython.js"></script>
+            <link type="text/css" rel="stylesheet" href="http://alexgorbatchev.com/pub/sh/current/styles/shCoreDefault.css"/>
+            <script type="text/javascript">SyntaxHighlighter.defaults.toolbar=false; SyntaxHighlighter.all();</script>
+    """
+
+    required_arguments = 1
+    optional_arguments = 0
+    has_content = True
+
+    def run(self):
+        self.assert_has_content()
+        self.options['brush'] = self.arguments[0]
+        html = '''\
+
+<pre class="brush: %s">
+%s
+</pre>
+
+'''
+        return [nodes.raw('', html % (self.options['brush'],
+            "\n".join(self.content).replace('<', '&lt;')),
+            format='html')]
+
+
+def makeExtension():
+    return Code
---
+++ acrylamid/filters/rstx_sourcecode.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+#
+# Copyright 2012 posativ <info@posativ.org>. All rights reserved.
+# License: BSD Style, 2 clauses. see acrylamid/__init__.py
+
+from docutils import nodes
+from docutils.parsers.rst import directives, Directive
+
+from pygments import highlight
+from pygments.formatters import HtmlFormatter
+from pygments.lexers import get_lexer_by_name, TextLexer
+
+match = ['code-block', 'sourcecode', 'pygments']
+
+
+class Pygments(Directive):
+    """ Source code syntax hightlighting using Pygments.
+
+    Usage example::
+
+        .. sourcecode:: python
+            :linenos:
+
+            #!/usr/bin/env python
+            print "Hello World!
+
+    ``linenos`` enables line numbering. A pygment CSS is not provided by
+    acrylamid but can easily get one by using the ``pygmentize`` script
+    shipped with Pygments.
+
+    ::
+
+        $> pygmentize -S trac -f html > pygments.css
+
+    To get a list of all available styles use the interactive python interpreter.
+
+    ::
+
+        >>> from pygments import styles
+        >>> print list(styles.get_all_styles())
+    """
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = True
+    has_content = True
+    option_spec = {
+        'linenos': directives.flag,
+    }
+
+    def run(self):
+        self.assert_has_content()
+        try:
+            lexer = get_lexer_by_name(self.arguments[0])
+        except ValueError:
+            # no lexer found - use the text one instead of an exception
+            lexer = TextLexer()
+        formatter = HtmlFormatter(noclasses=False)
+        if 'linenos' in self.options:
+            formatter.linenos = 2
+        parsed = highlight(u'\n'.join(self.content), lexer, formatter)
+        return [nodes.raw('', parsed, format='html')]
+
+
+def makeExtension():
+    return Pygments
