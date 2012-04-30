posativ/acrylamid

commit d6dc8f6dc4d97c3f7050c683cac01016930c0456

use a single, compressed cache file for each entry
Instead of creating for every intermediate/output a new file, we
now save all computed versions (zlib-compressed) of an entry into
a single file. I did not experienced any performance impact but
much smaller amount of cache files (163 versus 931).

acrylamid/utils.py
@@ -10,6 +10,7 @@ import codecs
 import tempfile
 import subprocess
 import hashlib
+import zlib
 from fnmatch import fnmatch
 from datetime import datetime
 from os.path import join, exists, dirname, getmtime, basename
@@ -263,7 +264,6 @@ class FileEntry:
                         remap={'tag': 'tags', 'filter': 'filters'})
         self.offset = i
         self.props.update(props)
-        self.ctime = 0.01  # time used to compile (cheating with 0.01 init value)
 
         fx = self.props.get('filters', [])
         if isinstance(fx, basestring):
@@ -272,7 +272,7 @@ class FileEntry:
         self.filters = FilterTree(fx)
 
     def __repr__(self):
-        return "<fileentry f'%s'>" % self.filename
+        return "<FileEntry f'%s'>" % self.filename
 
     @cached_property
     def permalink(self):
@@ -360,21 +360,29 @@ class FileEntry:
 
     @property
     def content(self):
+        """Returns the processed content.  This one of the core functions of
+        acrylamid: it compiles incrementally the filter chain using a tree
+        representation and saves final output or intermediates to cache, so
+        we can rapidly re-compile the whole content.
+
+        The cache is rather dumb: acrylamid can not determine wether it's
+        abandoned or differs only in a single character. Thus, to minimize
+        the overhead the content is zlib-compressed."""
 
         # previous value
         pv = None
 
         for i, fxs in self.filters.iter(context=self.context):
-            hv = md5(self.filename, i, fxs)
+            key = md5(i, fxs)
+            obj = md5(self.filename)
 
             try:
-                rv = cache.get(hv, mtime=self.mtime)
+                rv = cache.get(obj, key, mtime=self.mtime)
                 if rv is None:
                     res = self.source if pv is None else pv
                     for f in fxs:
                         res = f.transform(res, self, *f.args)
-                    pv = cache.set(hv, res)
-                    # XXX
+                    pv = cache.set(obj, key, data=res)
                     self.has_changed = True
                 else:
                     pv = rv
@@ -400,7 +408,7 @@ class FileEntry:
     @property
     def has_changed(self):
         i, fxs = list(self.filters.iter(self.context))[-1]
-        path = md5(self.filename, i, fxs)
+        path = md5(self.filename)
 
         if not exists(cache._get_filename(path)):
             return True
@@ -734,32 +742,61 @@ class cache(object):
                 pass
 
     @classmethod
-    def get(self, key, default=None, mtime=0.0):
+    def get(self, obj, key, default=None, mtime=0.0):
+        """Restore data from obj[key] if mtime has not changed or return default.
+
+        :param obj: object's hash that will be used as filename
+        :param key: key of this data
+        :param default: default return value
+        :param mtime: modification timestamp as float value
+        """
         try:
-            filename = self._get_filename(key)
+            filename = self._get_filename(obj)
             if mtime > getmtime(filename):
                 return default
             with open(filename, 'rb') as fp:
-                return pickle.load(fp)
+                return zlib.decompress(pickle.load(fp)[key])
             os.remove(filename)
-        except (OSError, IOError, pickle.PickleError):
+        except (OSError, IOError, KeyError, pickle.PickleError, zlib.error):
             pass
         return default
 
     @classmethod
-    def set(self, key, value):
-        filename = self._get_filename(key)
-        try:
-            fd, tmp = tempfile.mkstemp(suffix=self._fs_transaction_suffix,
-                                       dir=self.cache_dir)
-            with os.fdopen(fd, 'wb') as fp:
-                pickle.dump(value, fp, pickle.HIGHEST_PROTOCOL)
-            os.rename(tmp, filename)
-            os.chmod(filename, self.mode)
-        except (IOError, OSError):
-            pass
+    def set(self, obj, key, data):
+        """Save a key, value pair into a blob using pickle and moderate zlib
+        compression (level 6). We simply save a dictionary containing all
+        different intermediates (from every view) of an entry.
 
-        return value
+        :param obj: the object's hash, used as filename
+        :param key: dictionary key where we store the data
+        :param data: a string we compress with zlib and afterwards save
+        """
+        filename = self._get_filename(obj)
+
+        if exists(filename):
+            try:
+                with open(filename, 'rb') as fp:
+                    rv = pickle.load(fp)
+            except pickle.PickleError:
+                rv = {}
+            try:
+                with open(filename, 'wb') as fp:
+                    rv[key] = zlib.compress(data, 6)
+                    pickle.dump(rv, fp, pickle.HIGHEST_PROTOCOL)
+            except (IOError, OSError):
+                pass
+        else:
+            try:
+                fd, tmp = tempfile.mkstemp(suffix=self._fs_transaction_suffix,
+                                           dir=self.cache_dir)
+                with os.fdopen(fd, 'wb') as fp:
+                    pickle.dump({key: zlib.compress(data, 6)}, fp, pickle.HIGHEST_PROTOCOL)
+                os.rename(tmp, filename)
+                os.chmod(filename, self.mode)
+            except (IOError, OSError):
+                pass
+
+        return data
 
     @classmethod
     def get_mtime(self, key, default=0.0):

commit f9160308a5cba6723416bdd9a331e354a263fd71

add filter version, close #26
When a filter fixes an issue, acrylamid automatically re-compiles
all affected entries.

This fixes also a regression introduced with d6dc8f6dc4 that did
ignore modifications in the array of filters (in given context)
so that an article gets marked as *has_changed*.

Now an article is marked as has_changed when one or more intermediates
(including the final result) are missing, the cache file does not exist
or the mtime of entry.filename > cache's filename.

acrylamid/filters/__init__.py
@@ -129,6 +129,8 @@ class meta(type):
 class Filter(object):
 
     __metaclass__ = meta
+
+    version = "1.0.0"
     priority = 50.0
     conflicts = []
 
@@ -138,8 +140,8 @@ class Filter(object):
         self.args = args
 
     def __repr__(self):
-        return "<%s %2.f:%s>" % (self.__class__.__name__,
-                                 self.priority, self.name)
+        return "<%s@%s %2.f:%s>" % (self.__class__.__name__, self.version,
+                                    self.priority, self.name)
 
     def __hash__(self):
         return hash(self.name + repr(self.args))

acrylamid/filters/acronyms.py
@@ -17,7 +17,10 @@ from acrylamid.filters import Filter
 class Acronyms(Filter):
 
     match = [re.compile('^Acronyms?$', re.I), 'abbr', 'Abbr']
-    priority = 20.0  # after Typography, so CAPS is around ABBR
+    version = '1.0.0'
+
+    # after Typography, so CAPS is around ABBR
+    priority = 20.0
 
     def init(self, conf, env):
 

acrylamid/filters/head_offset.py
@@ -10,6 +10,7 @@ class Headoffset(Filter):
     this filter, e.g. `h2' increases headers by two."""
 
     match = ['h' + str(i + 1) for i in range(5)]
+    version = '1.0.0'
 
     def transform(self, text, entry, *args):
 

acrylamid/filters/html.py
@@ -10,6 +10,8 @@ from acrylamid.filters import Filter
 class HTML(Filter):
 
     match = [re.compile('^(pass|plain|X?HTML)$', re.I)]
+    version = '1.0.0'
+
     conflicts = ['rst', 'md']
     priority = 70.0
 

acrylamid/filters/hyphenation.py
@@ -156,6 +156,7 @@ def build(lang):
 class Hyphenate(Filter):
 
     match = [re.compile('^(H|h)yph')]
+    version = '1.0.0'
 
     @cached_property
     def default(self):

acrylamid/filters/jinja2-templating.py
@@ -14,6 +14,8 @@ class Jinja2(Filter):
     written posts. XXX: and offers some jinja2 extensions."""
 
     match = ['Jinja2', 'jinja2']
+    version = '1.0.0'
+
     priority = 90.0
 
     def init(self, conf, env, *args):

acrylamid/filters/md.py
@@ -14,6 +14,8 @@ from acrylamid.errors import AcrylamidException
 class Markdown(Filter):
 
     match = ['md', 'mkdown', 'markdown', 'Markdown']
+    version = '1.0.0'
+
     conflicts = ['rst', 'plain']
     priority = 70.0
 

acrylamid/filters/metalogo.py
@@ -8,6 +8,8 @@ from acrylamid.errors import AcrylamidException
 class Tex(Filter):
 
     match = ['metalogo']
+    version = '1.0.0'
+
     priority = 70.0
 
     def transform(self, text, entry, *args):

acrylamid/filters/pandoc.py
@@ -9,6 +9,8 @@ from acrylamid.errors import AcrylamidException
 class Pandoc(Filter):
 
     match = ['Pandoc', 'pandoc']
+    version = '1.0.0'
+
     conflicts = ['Markdown', 'reStructuredText', 'HTML']
     priority = 70.0
 

acrylamid/filters/pytextile.py
@@ -13,6 +13,8 @@ except ImportError:
 class PyTextile(Filter):
 
     match = ['Textile', 'textile', 'pytextile', 'PyTextile']
+    version = '1.0.0'
+
     conflicts = ['Markdown', 'reStructuredText', 'HTML', 'Pandoc']
     priority = 70.0
 

acrylamid/filters/rst.py
@@ -13,6 +13,8 @@ from acrylamid.utils import cached_property
 class Restructuredtext(Filter):
 
     match = ['restructuredtext', 'rst', 'rest', 'reST', 'reStructuredText']
+    version = '1.0.0'
+
     conflicts = ['markdown', 'plain']
     priority = 70.00
 

acrylamid/filters/summarize.py
@@ -78,6 +78,7 @@ class Summarize(Filter):
     """Summarizes content up to `maxwords` (defaults to 100)."""
 
     match = ['summarize', 'sum']
+    version = '1.0.0'
 
     def init(self, conf, env):
 

acrylamid/filters/typography.py
@@ -28,6 +28,8 @@ default = ['amp', 'widont', 'smartypants', 'caps']
 class Typography(Filter):
 
     match = [re.compile('^(T|t)ypo(graphy)?$'), 'smartypants']
+    version = '1.0.0'
+
     priority = 25.0
 
     def init(self, conf, env):

acrylamid/utils.py
@@ -439,15 +439,22 @@ class FileEntry:
 
     @property
     def has_changed(self):
-        i, fxs = list(self.filters.iter(self.context))[-1]
-        path = md5(self.filename)
+        """Check wether the entry has changed using the following conditions:
 
-        if not exists(cache._get_filename(path)):
-            return True
-        if getmtime(self.filename) > cache.get_mtime(path):
-            return True
+        - cache file does not exist -> has changed
+        - cache file does not contain required filter intermediate -> has changed
+        - entry's file is newer than the cache's one -> has changed
+        - otherwise -> not changed
+        """
+
+        path = md5(self.filename)
+        filters = self.filters.iter(self.context)
+
+        for i, fxs in filters:
+            if not cache.has_key(path, md5(i, fxs)):
+                return True
         else:
-            return False
+            return getmtime(self.filename) > cache.get_mtime(path)
 
     def keys(self):
         return list(iter(self))
@@ -772,6 +779,15 @@ class cache(object):
                 os.remove(fname)
             except (IOError, OSError):
                 pass
+
+    @classmethod
+    def has_key(self, obj, key):
+        try:
+            filename = self._get_filename(obj)
+            with open(filename, 'rb') as fp:
+                return key in pickle.load(fp)
+        except (OSError, IOError, pickle.PickleError):
+            return False
 
     @classmethod
     def get(self, obj, key, default=None, mtime=0.0):