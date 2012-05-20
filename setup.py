48f0dca9dd71378171fac440ba0ad1f4ef480467
posativ <info@posativ.org>
2012-05-21 01:20:52 +0200

    oh wait, experimental python 3.2 support
    Importing from an URL may fail due an UnicodeDecodeError (yai),
    and in general I did not run a single test. But compiling works
    so far.

--- setup.py
+++ setup.py
@@ -50,6 +50,11 @@ setup(
         "Intended Audience :: End Users/Desktop",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
     ],
     install_requires=requires,
     tests_require=[
