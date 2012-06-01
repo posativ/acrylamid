cf01bbd0ea402da5da03f046d20b1fd17cb09e72
posativ <info@posativ.org>
2012-06-01 12:55:17 +0200

    add ANSI styles like bold
        >>> from acrylamid.colors import red, bold
        assert red(bold(1)) == bold(red(1))

--- acrylamid/colors.py
+++ acrylamid/colors.py
@@ -3,14 +3,48 @@
 # Copyright 2012 posativ <info@posativ.org>. All rights reserved.
 # License: BSD Style, 2 clauses. see acrylamid/__init__.py
 
-def colorize(obj, m, n):
-    if not isinstance(obj, basestring):
-        obj = unicode(obj)
-    return '\033[%i;%im' % (m, n) + obj + '\033[0m'
+
+class ANSIString(object):
+
+    style = 0
+    color = 0
+
+    def __init__(self, obj, style=None, color=None):
+
+        if isinstance(obj, ANSIString):
+            if style is None:
+                style = obj.style
+            if color is None:
+                color = obj.color
+            obj = obj.obj
+        elif not isinstance(obj, basestring):
+            obj = unicode(obj)
+
+        self.obj = obj
+        if style:
+            self.style = style
+        if color:
+            self.color = color
+
+    def __unicode__(self):
+        return '\033[%i;%im' % (self.style, self.color) + self.obj + '\033[0m'
+
+    def __str__(self):
+        return unicode(self).decode('utf-8')
+
+    def __add__(self, other):
+        return unicode.__add__(unicode(self), other)
 
 
-red    = lambda obj: colorize(obj, 0, 31)
-green  = lambda obj: colorize(obj, 0, 32)
-yellow = lambda obj: colorize(obj, 0, 33)
-blue   = lambda obj: colorize(obj, 0, 34)
-white  = lambda obj: colorize(obj, 0, 37)
+normal = lambda obj: ANSIString(obj, style=0)
+bold   = lambda obj: ANSIString(obj, style=1)
+underline = lambda obj: ANSIString(obj, style=2)
+
+black  = lambda obj: ANSIString(obj, color=30)
+red    = lambda obj: ANSIString(obj, color=31)
+green  = lambda obj: ANSIString(obj, color=32)
+yellow = lambda obj: ANSIString(obj, color=33)
+blue   = lambda obj: ANSIString(obj, color=34)
+magenta= lambda obj: ANSIString(obj, color=35)
+cyan   = lambda obj: ANSIString(obj, color=36)
+white  = lambda obj: ANSIString(obj, color=37)
