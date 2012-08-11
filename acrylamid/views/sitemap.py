f72a6adfdb527a2910d26aab38ba8d2039e32c4a
posativ <info@posativ.org>
2012-08-11 23:44:10 +0200

    fix ambiguous url detection in sitemap view
    Urls for Atom and RSS feed look like regular static page urls but
    should not get a 1.0 priority with a changefreq of `never'.
    
    Now sitemap creates patterns for all active views can score them
    more individually (still a minor TODO).

--- acrylamid/views/sitemap.py
+++ acrylamid/views/sitemap.py
@@ -22,14 +22,14 @@ class Map(object):
         self.sm.write(u"<?xml version='1.0' encoding='UTF-8'?>\n")
         self.sm.write(u'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n')
 
-    def add(self, url, lastmod, changefreq='never', priority=None):
+    def add(self, url, lastmod, changefreq='never', priority=0.5):
 
         self.sm.write(u'  <url>\n')
         self.sm.write(u'    <loc>%s</loc>\n' % url)
         self.sm.write(u'    <lastmod>%s</lastmod>\n' % strftime('%Y-%m-%d', gmtime(lastmod)))
         if changefreq:
             self.sm.write(u'    <changefreq>%s</changefreq>\n' % changefreq)
-        if priority:
+        if priority != 0.5:
             self.sm.write(u'    <priority>%.1f</priority>\n' % priority)
         self.sm.write(u'  </url>\n')
 
@@ -50,6 +50,7 @@ class Sitemap(View):
     """
 
     priority = 0.0
+    scores = {'page': (1.0, 'never'), 'entry': (1.0, 'never')}
 
     def init(self):
 
@@ -68,25 +69,42 @@ class Sitemap(View):
         event.register(changed, to=['create', 'update', 'identical'])
 
     def context(self, env, request):
+        """"Here we prepare the detection pattern and active views. For each view we convert
+        ``view.path`` to a regular expression pattern using simple replacements."""
 
         patterns = dict()
-        replacements = [(':year', '\d+'), (':month', '\d+'), (':day', '\d+'), (':[^/]+', '\S+')]
+        replacements = [(':year', '\d+'), (':month', '\d+'), (':day', '\d+'), (':[^/]+', '[^/]+')]
 
-        for fmt in ('entry_permalink', 'page_permalink'):
-            permalink = self.conf[fmt]
+        for name, view in self.env.views.iteritems():
+            permalink = view.path
             for pat, repl in replacements:
                 permalink = re.sub(pat, repl, permalink)
 
-                # we rank full entries higher
-                patterns[fmt] = re.compile(joinurl(re.escape(self.conf['www_root']), permalink))
+            if permalink.endswith('/'):
+                permalink += 'index.html'
 
-        self.isfullentry = lambda url: patterns['entry_permalink'].match(url) or \
-                                       patterns['page_permalink'].match(url)
+            patterns[name] = re.compile('^' + joinurl(re.escape(self.conf['www_root']), permalink) + '$')
+
+        self.patterns = patterns
+        self.views = []
+
+        # sort active views by frequency
+        views = env.views.keys()
+        for v in 'entry', 'tag', 'index':
+            try:
+                self.views.append(views.pop(views.index(v)))
+            except IndexError:
+                pass
+        self.views.extend(views)
 
     def generate(self, request):
+        """In this step, we filter drafted entries (they should not be included into the
+        Sitemap) and test each url pattern for success and write the corresponding
+        changefreq and priority to the Sitemap."""
 
         drafted = set([joinurl(self.conf['output_dir'], e.permalink, 'index.html')
                     for e in request['entrylist'] + request['pages'] if e.draft])
+
         path = joinurl(self.conf['output_dir'], self.path)
         sm = Map()
 
@@ -99,11 +117,12 @@ class Sitemap(View):
                 continue
 
             url = join(self.conf['www_root'], fname.replace(self.conf['output_dir'], ''))
+            for view in self.views:
 
-            clean_url = rchop(url, 'index.html')
-            if self.isfullentry(url):
-                sm.add(clean_url, getmtime(fname), priority=1.0)
-            else:
-                sm.add(clean_url, getmtime(fname), changefreq='weekly')
+                if self.patterns[view].match(url):
+                    priority, changefreq = self.scores.get(view, (0.5, 'weekly'))
+                    sm.add(rchop(url, 'index.html'), getmtime(fname), changefreq, priority)
+
+                    break
 
         yield sm.read(), path
