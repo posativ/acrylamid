fbfaf2e4aa6bdc3715c98a6fce97617ad8a6e8b5
posativ <info@posativ.org>
2012-09-08 19:54:07 +0200

    support for LESS, SASS and SCSS

--- acrylamid/assets/__init__.py
+++ acrylamid/assets/__init__.py
@@ -3,22 +3,19 @@
 # Copyright 2012 posativ <info@posativ.org>. All rights reserved.
 # License: BSD Style, 2 clauses. see acrylamid/__init__.py
 
+import os
 import io
+import time
 
+from tempfile import mkstemp
 from os.path import join, relpath, isfile, getmtime, splitext
 
-from acrylamid import readers, utils
+from acrylamid import core, readers, helpers
+from acrylamid.errors import AcrylamidException
 from acrylamid.helpers import mkfile, event
 
 __writers = None
 __defaultwriter = None
-
-__ext_map = {
-    # '.sass': 'acrylamid.assets.SASSWriter',
-    # '.less': 'acrylamid.assets.LESSWriter',
-    # '.haml': 'acrylamid.assets.HAMLWriter',
-    # ...
-}
 
 
 class DefaultWriter(object):
@@ -30,6 +27,52 @@ class DefaultWriter(object):
 
         with io.open(src, 'rb') as fp:
             mkfile(fp, dest, force=force, dryrun=dryrun, mode="b")
+
+
+class System(DefaultWriter):
+
+    def write(self, src, dest, force=False, dryrun=False):
+
+        dest = dest.replace(self.ext, self.target)
+        if not force and isfile(dest) and getmtime(dest) > getmtime(src):
+            return event.skip(dest)
+
+        if isinstance(self.cmd, basestring):
+            self.cmd = [self.cmd, ]
+
+        tt = time.time()
+        fd, path = mkstemp(dir=core.cache.cache_dir)
+
+        try:
+            res = helpers.system(self.cmd + [src])
+        except OSError:
+            if isfile(dest):
+                os.unlink(dest)
+            raise AcrylamidException('%s is not available!' % self.cmd[0])
+
+        with os.fdopen(fd, 'w') as fp:
+            fp.write(res)
+
+        with io.open(path, 'rb') as fp:
+            mkfile(fp, dest, ctime=time.time()-tt, force=force, dryrun=dryrun, mode="b")
+
+
+class SASSWriter(System):
+
+    ext, target = '.sass', '.css'
+    cmd = ['sass', ]
+
+
+class SCSSWriter(System):
+
+    ext, target = '.scss', '.css'
+    cmd = ['sass', '--scss']
+
+
+class LESSWriter(System):
+
+    ext, target = '.less', '.css'
+    cmd = ['lessc', ]
 
 
 def initialize(conf, env):
@@ -46,6 +89,14 @@ def compile(conf, env):
 
     global __writers, __default
 
+    ext_map = {
+        '.sass': SASSWriter,
+        '.scss': SCSSWriter,
+        '.less': LESSWriter,
+        # '.haml': 'HAMLWriter',
+        # ...
+    }
+
     files = (relpath(path, conf['theme']) for path in readers.filelist(conf['theme']))
     files = (f for f in files if f not in env.engine.templates)
 
@@ -53,8 +104,8 @@ def compile(conf, env):
 
         # initialize writer for extension if not already there
         _, ext = splitext(path)
-        if ext in __ext_map and ext not in __writers:
-            __writers[ext] = utils.import_object(__ext_map[ext])()
+        if ext in ext_map and ext not in __writers:
+            __writers[ext] = ext_map[ext]()
 
         src, dest = join(conf['theme'], path), join(conf['output_dir'], path)
         writer = __writers.get(ext, __defaultwriter)
