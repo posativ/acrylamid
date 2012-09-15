d246af349da608146d559e91a0c7f5eacce008a3
posativ <info@posativ.org>
2012-09-15 12:08:14 +0200

    add tests for mako, head_offset and acronym filter

--- specs/test_filters_builtin.py
+++ specs/test_filters_builtin.py
@@ -51,37 +51,76 @@ class Hyphenation(unittest.TestCase):
         assert hyphenate('Airplane') == ['Air', 'plane']
 
 
-class Jinja2(unittest.TestCase):
+def test_jinja2():
 
-    def test_works(self):
+    jinja2 = get_filters()['Jinja2'](conf, env, 'Jinja2')
 
-        jinja2 = get_filters()['Jinja2'](conf, env, 'Jinja2')
-
-        assert jinja2.transform('{{ entry.lang }}', Entry('de')) == 'de'
-        assert jinja2.transform("{{ 'which which' | system }}", None) == '/usr/bin/which'
+    assert jinja2.transform('{{ entry.lang }}', Entry('de')) == 'de'
+    assert jinja2.transform("{{ 'which which' | system }}", None) == '/usr/bin/which'
 
 
-class Summarize(unittest.TestCase):
+def test_mako():
 
-    def test_works(self):
+    mako = get_filters()['Mako'](conf, env, 'Mako')
 
-        summarize = get_filters()['summarize'](conf, env, 'summarize')
-        examples = [('Hello World', 'Hello World'),
-                    # a real world example
-                    ('<p>Hello World, you have to click this link because</p>',
-                     '<p>Hello World, you have to <span>&#8230;<a href="/foo/" '+ \
-                     'class="continue">continue</a>.</span></p>'),
-                    ('<p>Hel&shy;lo Wor&shy;ld, you have to click this link because</p>',
-                    # now with HTML entities
-                     '<p>Hel&shy;lo Wor&shy;ld, you have to <span>&#8230;<a href="/foo/" '+ \
-                     'class="continue">continue</a>.</span></p>'),
-                    ('Hello<br />', 'Hello<br />'),
-                    ('<p>Hello World, you have<br /> to <br /> click<br /> this<br /> link...</p>',
-                     '<p>Hello World, you have<br /> to <span>&#8230;<a href="/foo/" '+ \
-                     'class="continue">continue</a>.</span></p>'),
-                    ('Hello World, you have to click this link because',
-                     'Hello World, you have to <span>&#8230;<a href="/foo/" '+ \
-                     'class="continue">continue</a>.</span>')]
+    e = Entry('de')
+    e.filename = '1'
 
-        for text, result in examples:
-            assert summarize.transform(text, Entry(), '5') == result
+    assert mako.transform('${entry.lang}', Entry('de')) == 'de'
+    assert mako.transform("${ 'which which' | system }", e) == '/usr/bin/which'
+
+
+def test_acronyms():
+
+    acronyms = get_filters()['Acronyms'](conf, env, 'Acronyms')
+    abbr = lambda abbr, expl: '<abbr title="%s">%s</abbr>' % (expl, abbr)
+
+    examples = [
+        ('CGI', abbr('CGI', 'Common Gateway Interface')),
+        ('IMDB', abbr('IMDB', 'Internet Movie Database')),
+        ('IMDb', abbr('IMDb', 'Internet Movie Database')),
+        ('PHP5', abbr('PHP5', 'Programmers Hate PHP ;-)')),
+        ('TEST', 'TEST')
+    ]
+
+    for test, result in examples:
+        assert acronyms.transform(test, None) == result
+
+def test_headoffset():
+
+    h1 = get_filters()['h1'](conf, env, 'h1')
+    examples = [
+        ('<h1>Hello</h1>', '<h2>Hello</h2>'), ('<h2>World</h2>', '<h3>World</h3>'),
+        ('<h1 class="foo bar">spam</h1>', '<h2 class="foo bar">spam</h2>'),
+        ('<h1 class="foo" id="baz">spam</h1>', '<h2 class="foo" id="baz">spam</h2>'),
+    ]
+
+    for test, result in examples:
+        assert h1.transform(test, None) == result
+
+    h5 = get_filters()['h5'](conf, env, 'h5')
+    assert h5.transform('<h3>eggs</h3>', '<h6>eggs</h6>')
+
+
+def test_summarize():
+
+    summarize = get_filters()['summarize'](conf, env, 'summarize')
+    examples = [('Hello World', 'Hello World'),
+                # a real world example
+                ('<p>Hello World, you have to click this link because</p>',
+                 '<p>Hello World, you have to <span>&#8230;<a href="/foo/" '+ \
+                 'class="continue">continue</a>.</span></p>'),
+                ('<p>Hel&shy;lo Wor&shy;ld, you have to click this link because</p>',
+                # now with HTML entities
+                 '<p>Hel&shy;lo Wor&shy;ld, you have to <span>&#8230;<a href="/foo/" '+ \
+                 'class="continue">continue</a>.</span></p>'),
+                ('Hello<br />', 'Hello<br />'),
+                ('<p>Hello World, you have<br /> to <br /> click<br /> this<br /> link...</p>',
+                 '<p>Hello World, you have<br /> to <span>&#8230;<a href="/foo/" '+ \
+                 'class="continue">continue</a>.</span></p>'),
+                ('Hello World, you have to click this link because',
+                 'Hello World, you have to <span>&#8230;<a href="/foo/" '+ \
+                 'class="continue">continue</a>.</span>')]
+
+    for text, result in examples:
+        assert summarize.transform(text, Entry(), '5') == result
