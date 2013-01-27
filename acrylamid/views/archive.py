a797326329eaac68ff2efc2c7ebccce1418c0fe1
Martin Zimmermann <info@posativ.org>
2013-01-27 20:38:10 +0100

    env.archive object to access current year and optionally month and day
    .. code-block:: html+jinja2
    
        <h2>Archives for {{ env.archive.year ~ '/' ~ env.archive.month }}</h2>

--- acrylamid/views/archive.py
+++ acrylamid/views/archive.py
@@ -49,12 +49,17 @@ class Archive(View):
             route = joinurl(conf['output_dir'], expand(self.path, group[0]))
             path  = joinurl(route, 'index.html')
 
+            # an object storing year, zero-padded month and day as attributes (may be None)
+            key = type('Archive', (object, ), dict(zip(('year', 'month', 'day'),
+                map(lambda x, y: '%02i' % (x + y) if y else None, *((0, 0, 0), keyfunc(group[0])))
+            )))()
+
             if isfile(path) and not (modified or tt.modified or env.modified or conf.modified):
                 event.skip(path)
                 continue
 
             html = tt.render(conf=conf, env=union(env, entrylist=group,
                 type='archive', prev=prev, curr=link(route), next=next,
-                num_entries=len(group), route=path))
+                num_entries=len(group), route=path, archive=key))
 
             yield html, path
