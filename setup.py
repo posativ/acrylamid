b2564124566c850f249eeca890a37cf509490132
Martin Zimmermann <info@posativ.org>
2013-05-05 22:20:48 +0200

    Preparing release 0.7.3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.platform == 'win32':
 
 setup(
     name='acrylamid',
-    version='0.7.3.dev0',
+    version='0.7.3',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
