ec14b7e8669b662cddddbd7fabc40f788d349e18
Martin Zimmermann <info@posativ.org>
2014-03-16 14:52:33 +0100

    Back to development: 0.7.9

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if sys.platform == 'win32':
 
 setup(
     name='acrylamid',
-    version='0.7.8',
+    version='0.7.9.dev0',
     author='Martin Zimmermann',
     author_email='info@posativ.org',
     packages=find_packages(),
