ebcaa93970fd7ee4a42a2b86b6e5b7d9a762f1ef
blubee <gurenchan@gmail.com>
2015-01-24 01:06:52 +0800

    cleaning up again

--- acrylamid/errors.py
+++
@@ -1,8 +0,0 @@
-# -*- encoding: utf-8 -*-
-#
-# Copyright 2012 Martin Zimmermann <info@posativ.org>. All rights reserved.
-# License: BSD Style, 2 clauses -- see LICENSE.
-
-
-class AcrylamidException(Exception):
-    pass
--- docs/api/readers.rst
+++
@@ -1,9 +0,0 @@
-==============
-Reader Objects
-==============
-
-.. automethod:: acrylamid.readers.load
-
-.. autoclass:: acrylamid.readers.Entry
-   :members:
-   :inherited-members:
