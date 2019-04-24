f8587aea9921cd64d404d7e32f31256b1a1198a4
Farley Reynolds <farleykreynolds@gmail.com>
2019-04-24 17:05:10 -0400

    remove check for Python 2, import htmlparser

--- acrylamid/lib/html.py
+++ acrylamid/lib/html.py
@@ -23,12 +23,8 @@ import re
 from cgi import escape
 from acrylamid.compat import PY2K, unichr
 
-if PY2K:
-    from HTMLParser import HTMLParser as DefaultParser, HTMLParseError
-    from htmlentitydefs import name2codepoint
-else:
-    from html.parser import HTMLParser as DefaultParser, HTMLParseError
-    from html.entities import name2codepoint
+import HTMLParser as DefaultParser, HTMLParseError
+from html.entities import name2codepoint
 
 
 def unescape(s):
