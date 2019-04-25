ab3b8aab4cd4737a44464511d8ce398b4ce8805b
Farley Reynolds <farleykreynolds@gmail.com>
2019-04-25 13:35:44 -0400

    fix flake8 errors

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import re
 
 import pathlib
 from setuptools import setup, find_packages
--- specs/lib.py
+++ specs/lib.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# flake8: noqa
 
 from attest import test, TestBase
 
